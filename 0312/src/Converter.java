19bf577d0c48c24952119d6ba077aec6f091ea8c
pongdang04 <pongdangdang419@gmail.com>
2025-03-12 22:34:51 +0900

    second-> min, hour, day converter

---
+++ 0312/src/Converter.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Converter {
+
+	public static void main(String[] args) {
+		int isec, imin, ihour, iday, iTotalSec;
+		
+		Scanner oInDev;
+		oInDev= new Scanner(System.in);
+		
+		System.out.printf("Enter posititve second: ");
+		iTotalSec=oInDev.nextInt();
+		
+		isec=iTotalSec%60;
+		imin=iTotalSec/60;
+		
+		ihour=imin/60;
+		imin=imin%60;
+		
+		iday=iTotalSec%24;
+		
+		System.out.print(iday +"일" + ihour + "시간" + imin+"분" +isec+"초");
+		//System.out.printf("%d일 %d시간 %d분 %d초", iday, ihour, imin, isec);
+		
+		oInDev.close();
+	}
+
+}
