c51b7bc5bcc06f418ea3d99cd4510d0d43f217b1
pongdang04 <pongdangdang419@gmail.com>
2025-03-12 22:33:57 +0900

    Settings of Cafe && menu board

---
+++ 0310project/src/CMyMiniCafe.java
@@ -0,0 +1,14 @@
+public class CMyMiniCafe {
+	public static void main(String[] args) {
+	System.out.println("나의 작은 카페 메뉴");
+	System.out.println("=================");
+	System.out.println(); 
+	System.out.println("1. 에스프레소 1500원");
+	System.out.println("2. 아메리카노 2000원");
+	System.out.println("3. 카페라떼  2500원");
+	System.out.println("4. 아이스티  3000원");
+	System.out.println();
+	System.out.println("=================");
+	System.out.println("메뉴를 선택 (0:정산):");
+	}
+}
