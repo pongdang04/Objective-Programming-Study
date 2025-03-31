3cdffd0616e47d19e720a2e9c67caef3b013fff1
pongdang04 <pongdangdang419@gmail.com>
2025-03-31 11:35:24 +0900

    Selection Sort

---
+++ src/Ex2_0331_SelectSort.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Ex2_0331_SelectSort {
+    public static void main(String[] args) {
+        final int ARRAY_LENGTH = 8;
+        Scanner myScanner = new Scanner(System.in);
+        int[] inputArr = new int[ARRAY_LENGTH];
+        int tmpMin, minIdx, tmpSwitch;
+
+        System.out.print("Enter 8 numbers : ");
+        for(int i=0;i<ARRAY_LENGTH; i++){
+            inputArr[i]=myScanner.nextInt();
+        }
+
+        //Sort
+        for(int i=0;i<ARRAY_LENGTH;i++){
+            tmpMin = inputArr[i];
+            minIdx = i;
+            for(int j=i;j<ARRAY_LENGTH;j++){
+                if(inputArr[j]<tmpMin){
+                    tmpMin = inputArr[j];
+                    minIdx = j;
+                }
+            }
+            if(minIdx==i){
+                continue;
+            }else{
+                tmpSwitch = tmpMin;
+                inputArr[minIdx]= inputArr[i];
+                inputArr[i]= tmpSwitch;
+            }
+        }
+
+        //Print
+        for(int i=0;i< inputArr.length; i++){
+            System.out.print(inputArr[i] + " ");
+        }
+    }
+}
